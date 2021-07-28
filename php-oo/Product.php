0bcb7e829f711c193f56bce50358503bfa9b3b39
nely de faria <nelly.nf54@gmail.com>
2021-07-28 16:19:15 -0300

    Criando novos objetos

--- php-oo/Product.php
+++ php-oo/Product.php
@@ -16,6 +16,21 @@ $tv->price = 4.500;
 $tv->brand = 'Samsung';
 $tv->weight = 15;
 
-var_dump($tv);
+$geladeira = new Product;
+$geladeira->name = 'Geladeira';
+$geladeira->description = 'Degelo automático';
+$geladeira->price = 3.400;
+$geladeira->brand = 'Consul';
+$geladeira->weight = 334;
+
+
+$microondas = new Product;
+$microondas->name = 'Microondas';
+$microondas->description = '';
+$microondas->price = 400.00;
+$microondas->brand = 'Electrolux';
+$microondas->weight = 20;
+
+var_dump($tv, $geladeira, $microondas);
 
 // Even if you're on the right track, you'll get run over if you just sit there.
