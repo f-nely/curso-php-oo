f29d5f953a81f844232a2a8eb1118e59e293d6e6
nely de faria <nelly.nf54@gmail.com>
2021-07-29 12:27:26 -0300

    Refatorando código

--- php-oo/teste_conta.php
+++ php-oo/teste_conta.php
@@ -1,17 +1,20 @@
 <?php
 
+use Conta\ContaPessoaFisica;
+use Conta\ContaPessoaJuridica;
+
 require_once 'Conta.php';
 require_once 'ContaPessoaJuridica.php';
 require_once 'ContaPessoaFisica.php';
 
-$conta1 = new Conta\ContaPessoaFisica;
+$conta1 = new ContaPessoaFisica;
 $conta1->setName('Carlos Ferreira');
 $conta1->setNumber('132123142');
 $conta1->setCpf('123.456.789-00');
 
 echo $conta1->showData();
 
-$especializati = new Conta\ContaPessoaJuridica;
+$especializati = new ContaPessoaJuridica;
 $especializati->setName('EspecializaTi');
 $especializati->setNumber('233004780');
 $especializati->setCnpj('987.456.123-10');
