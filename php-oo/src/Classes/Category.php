ef4ae7712f8c671c9c09484806d28089058b44f6
nely de faria <nelly.nf54@gmail.com>
2021-07-29 16:49:10 -0300

    Adiciona arquivo Category.php

---
+++ php-oo/src/Classes/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Classes;
+
+class Category
+{
+    private string $name;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
