8a34dbe8695b624b6de867ea1e9a03a61a6af3e0
nely de faria <nelly.nf54@gmail.com>
2021-07-29 12:22:24 -0300

    Adiciona arquivo Person.php

---
+++ php-oo/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Especializati;
+
+class Person
+{
+    private string $name;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
