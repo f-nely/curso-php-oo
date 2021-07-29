0610603568d606189bf0aff2bfd934b919d2e204
nely de faria <nelly.nf54@gmail.com>
2021-07-29 12:23:00 -0300

    Adiciona arquivo teste_person.php

---
+++ php-oo/teste_person.php
@@ -0,0 +1,12 @@
+<?php
+
+use Especializati\Person;
+
+require_once 'Person.php';
+
+$person = new  Person;
+$person->setName('Carlos Ferreira');
+echo $person->getName();
+
+// laragon - What we fear doing most is usually what we most need to do.
+
