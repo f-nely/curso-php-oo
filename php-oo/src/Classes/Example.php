e376b7635fc8f4b51279de53e0311ea2a66ae948
nely de faria <nelly.nf54@gmail.com>
2021-08-11 16:58:08 -0300

    Adiciona arquivo Example.php

---
+++ php-oo/src/Classes/Example.php
@@ -0,0 +1,23 @@
+<?php
+
+class Example extends ExampleMain
+{
+    public $name = 'nelynely';
+    private $id;
+    protected $last_name;
+
+    public function method1(): void
+    {
+
+    }
+
+    protected function method2(): void
+    {
+
+    }
+
+    private function method3(): void
+    {
+
+    }
+}
